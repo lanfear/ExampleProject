9c9c704a6c3dfa96b3665c5cacb5d7f5f75afb18
Joseph M. Deming <j.deming@techsmith.com>
2015-10-21 19:18:51 -0400

    added initial project files

---
+++ ExampleProject/SupportingClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject
+{
+   public class SupportingClass
+   {
+      public static string FormatLogText( string text )
+      {
+         return string.Format( "[{0}] {1}", DateTime.Now, text );
+      }
+
+      public static void PrintLine( string text )
+      {
+         Console.WriteLine( FormatLogText( text ) );
+      }
+   }
+}
---
+++ ExampleProjectTests/SupportingClassTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExampleProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleProjectTests
+{
+   [TestClass]
+   public class SupportingClassTest
+   {
+      [TestMethod]
+      public void FormatLogText_OutputContainsExpectedString()
+      {
+         string _expectedStringContents = "Some test string";
+         string _actualString = SupportingClass.FormatLogText( "Some test string" );
+         Assert.IsTrue( _actualString.Contains( _expectedStringContents ) );
+      }
+   }
+}
